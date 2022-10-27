0d8aa6608acec7f96f445f1baa024067c0a742bf
sewunet-ab <sewunetabebaw92@gmail.com>
2022-10-27 17:11:43 +0300

    Atachementutil and serverless Done

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -4,3 +4,14 @@ import * as AWSXRay from 'aws-xray-sdk'
 const XAWS = AWSXRay.captureAWS(AWS)
 
 // TODO: Implement the fileStogare logic
+const s3 = new XAWS.S3({
+  signatureVersion: 'v4'
+})
+const bucketName = process.env.ATTACHMENT_S3_BUCKET
+export function getUploadUrl(imageId: string) {
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: imageId,
+    Expires: 300
+  })
+}
